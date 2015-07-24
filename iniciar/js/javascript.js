6442beb445ee97b3fde4f9b3436464dfba4a51fe
wilberoq <wylyoscco@gmail.com>
2015-07-23 22:30:29 -0500

    estes codigos nos marca los pulsos para el  juego

--- iniciar/js/javascript.js
+++ iniciar/js/javascript.js
@@ -1,1 +1,12 @@
 // aqui va el codgo para el juego de nave
+// estes codigos nos marca los pulsos para el  juego
+window.requestAnimFrame = (function () {     //informa al navegador que quieres realizar una animación.
+    return  window.requestAnimationFrame        ||
+        window.webkitRequestAnimationFrame  || // Chrome/Webkit
+        window.mozRequestAnimationFrame     ||  //Firefox
+        window.oRequestAnimationFrame       ||
+        window.msRequestAnimationFrame      || 
+        function ( /* function */ callback, /* DOMElement */ element) {
+window.setTimeout(callback, 1000 / 60); // Llama a una función o ejecuta un fragmento de código después del retraso especificado.
+        };
+})();
